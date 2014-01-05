b18883ce78806d5503fb2bf3ad080a4316489a03
Steffen Mueller <smueller@cpan.org>
2014-01-05 10:20:45 +0100

    Add header with THX-member related macros

---
+++ src/thx_member.h
@@ -0,0 +1,31 @@
+#ifndef _DEVEL_STATPROFILER_THX_MEMBER
+#define _DEVEL_STATPROFILER_THX_MEMBER
+
+// Defines macros for use in classes that keep a Perl threading
+// context around.
+//
+// Example:
+// class Foo {
+// public:
+//   Foo(pTHX) {
+//    SET_THX_MEMBER
+//   }
+//   void bar() {
+//     // use aTHX here implicitly or explicitly
+//   }
+// private:
+//   DECL_THX_MEMBER
+// }
+
+#include <EXTERN.h>
+#include <perl.h>
+
+#ifdef USE_ITHREADS
+#   define DECL_THX_MEMBER tTHX my_perl;
+#   define SET_THX_MEMBER this->my_perl = aTHX;
+#else
+#   define DECL_THX_MEMBER
+#   define SET_THX_MEMBER
+#endif
+
+#endif
