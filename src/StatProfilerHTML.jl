23ab42d21f46f1e92ac6ede8b6358a21d8e98199
Timo Kluck <tkluck@infty.nl>
2017-05-14 23:00:07 +0100

    Call statprofilehtml even if it isn't in the PATH

--- src/StatProfilerHTML.jl
+++ src/StatProfilerHTML.jl
@@ -5,41 +5,47 @@ export statprofilehtml
 using Base.Profile
 using Iterators
 
+beacon() = "dummy function to use functionloc for finding the path"
+pkg_basepath() = dirname(dirname(functionloc(beacon)[1]))
+
 function statprofilehtml(data::Array{UInt,1} = UInt[],litrace::Dict{UInt,Array{StackFrame,1}} = Dict{UInt,Array{StackFrame,1}}())
     if length(data) == 0
         (data, litrace) = Profile.retrieve()
     end
 
-    formatter, process =  open(`statprofilehtml -`, "w", STDOUT)
-    #formatter = open("profileoutput.log", "w")
+    basepath = pkg_basepath()
 
-    lastwaszero = false
-    for d in data
-        if d == 0
-            if !lastwaszero
-                write(formatter, "\n")
-            end
-            lastwaszero = true
-            continue
-        end
-        # I don't understand the semantics of having more than one value in the array. Take
-        # all of them
-        frames= litrace[d]
-        for frame in frames
-            if !frame.from_c
-                file = Base.find_source_file(string(frame.file))
-                func_line = frame.line
-                if !isnull(frame.linfo)
-                    linfo = get(frame.linfo)
-                    func_line = linfo.def.line - 1  # off-by-one difference between how StatProfiler and julia seem to map this
+    withenv("PERL5LIB" => "$basepath/perllib") do
+        formatter, process =  open(`$basepath/bin/statprofilehtml`, "w", STDOUT)
+
+        lastwaszero = false
+        for d in data
+            if d == 0
+                if !lastwaszero
+                    write(formatter, "\n")
                 end
+                lastwaszero = true
+                continue
+            end
+            # I don't understand the semantics of having more than one value in the array. Take
+            # all of them
+            frames= litrace[d]
+            for frame in frames
+                if !frame.from_c
+                    file = Base.find_source_file(string(frame.file))
+                    func_line = frame.line
+                    if !isnull(frame.linfo)
+                        linfo = get(frame.linfo)
+                        func_line = linfo.def.line - 1  # off-by-one difference between how StatProfiler and julia seem to map this
+                    end
 
-                write(formatter, "$(file)\t$(frame.line)\t$(frame.func)\t$(func_line)\n")
-                lastwaszero = false
+                    write(formatter, "$(file)\t$(frame.line)\t$(frame.func)\t$(func_line)\n")
+                    lastwaszero = false
+                end
             end
         end
+        close(formatter)
     end
-    close(formatter)
 end
 
 end
