53a45f5f821fab96893e55544f364c7aa12f8b46
Timo Kluck <tkluck@infty.nl>
2018-01-07 22:21:12 +0100

    Use open() as a context manager instead of managing the process manually

--- src/StatProfilerHTML.jl
+++ src/StatProfilerHTML.jl
@@ -20,35 +20,33 @@ function statprofilehtml(data::Array{UInt,1} = UInt[],litrace::Dict{UInt,Array{S
     perllib         = Pkg.dir("StatProfilerHTML", "perllib")
 
     withenv("PERL5LIB" => perllib) do
-        formatter, process =  open(`perl $statprofilehtml $sharepath`, "w", STDOUT)
-
-        lastwaszero = false
-        for d in data
-            if d == 0
-                if !lastwaszero
-                    write(formatter, "\n")
-                end
-                lastwaszero = true
-                continue
-            end
-            # I don't understand the semantics of having more than one value in the array. Take
-            # all of them
-            frames= litrace[d]
-            for frame in frames
-                if !frame.from_c
-                    file = Base.find_source_file(string(frame.file))
-                    func_line = frame.line
-                    with_value(frame.linfo) do linfo
-                        func_line = linfo.def.line - 1  # off-by-one difference between how StatProfiler and julia seem to map this
+        open(`perl $statprofilehtml $sharepath`, "w", STDOUT) do formatter
+            lastwaszero = false
+            for d in data
+                if d == 0
+                    if !lastwaszero
+                        write(formatter, "\n")
                     end
+                    lastwaszero = true
+                    continue
+                end
+                # I don't understand the semantics of having more than one value in the array. Take
+                # all of them
+                frames= litrace[d]
+                for frame in frames
+                    if !frame.from_c
+                        file = Base.find_source_file(string(frame.file))
+                        func_line = frame.line
+                        with_value(frame.linfo) do linfo
+                            func_line = linfo.def.line - 1  # off-by-one difference between how StatProfiler and julia seem to map this
+                        end
 
-                    write(formatter, "$(file)\t$(frame.line)\t$(frame.func)\t$(func_line)\n")
-                    lastwaszero = false
+                        write(formatter, "$(file)\t$(frame.line)\t$(frame.func)\t$(func_line)\n")
+                        lastwaszero = false
+                    end
                 end
             end
         end
-        close(formatter)
-        wait(process)
     end
 end
 
