19e85e98d351e3648cfa958b8f610c788298b1f1
Timo Kluck <tkluck@infty.nl>
2018-11-15 20:00:30 +0100

    Let Profile module handle the flatten operation
    It seems this has something to do with inlining.
    In any case, now that the module takes care of this,
    I don't have to worry about the semantics.

--- src/StatProfilerHTML.jl
+++ src/StatProfilerHTML.jl
@@ -24,6 +24,8 @@ function statprofilehtml(data::Array{UInt,1} = UInt[],litrace::Dict{UInt,Array{S
         (data, litrace) = Profile.retrieve()
     end
 
+    data, litrace = Profile.flatten(data, litrace)
+
     withenv("PERL5LIB" => perllib) do
         open(`perl $statprofilehtml_pl $sharepath`, "w", stdout) do formatter
             lastwaszero = true
@@ -35,21 +37,17 @@ function statprofilehtml(data::Array{UInt,1} = UInt[],litrace::Dict{UInt,Array{S
                     lastwaszero = true
                     continue
                 end
-                # I don't understand the semantics of having more than one value in the array. Take
-                # all of them
-                frames= litrace[d]
-                for frame in frames
-                  if !frame.from_c || from_c
-                        file = Base.find_source_file(string(frame.file))
-                        func_line = frame.line
-                        with_value(frame.linfo) do linfo
-                            func_line = linfo.def.line - 1  # off-by-one difference between how StatProfiler and julia seem to map this
-                        end
-
-                        file_repr = file == nothing ? "nothing" : file
-                        write(formatter, "$(file_repr)\t$(frame.line)\t$(frame.func)\t$(func_line)\n")
-                        lastwaszero = false
+                frame = litrace[d]
+                if !frame.from_c || from_c
+                    file = Base.find_source_file(string(frame.file))
+                    func_line = frame.line
+                    with_value(frame.linfo) do linfo
+                        func_line = linfo.def.line - 1  # off-by-one difference between how StatProfiler and julia seem to map this
                     end
+
+                    file_repr = file == nothing ? "nothing" : file
+                    write(formatter, "$(file_repr)\t$(frame.line)\t$(frame.func)\t$(func_line)\n")
+                    lastwaszero = false
                 end
             end
         end
